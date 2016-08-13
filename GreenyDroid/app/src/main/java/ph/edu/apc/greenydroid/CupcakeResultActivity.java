fd0bbb12938a526d25a8f9a8b48c9746320706a6
Jose Lorenzo Tadeo <renzotadeo26@gmail.com>
2016-08-13 19:42:25 +0800

    updated

--- GreenyDroid/app/src/main/java/ph/edu/apc/greenydroid/CupcakeResultActivity.java
+++ GreenyDroid/app/src/main/java/ph/edu/apc/greenydroid/CupcakeResultActivity.java
@@ -1,7 +1,10 @@
 package ph.edu.apc.greenydroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -15,13 +18,7 @@ public class CupcakeResultActivity extends AppCompatActivity {
         int c = 0;
         String name = StartActivity.name;
 
-        if(StartActivity.name.isEmpty()){
-            TextView Score = (TextView)findViewById(R.id.score);
-            Score.setText("Your score is: " + c);
-        }else{
-            TextView Name = (TextView)findViewById(R.id.textView);
-            Name.setText("Congratulation " + name);
-        }
+
 
         if(CCupcakeQuestionActivity.D.isChecked()){
             c+=1;
@@ -38,9 +35,27 @@ public class CupcakeResultActivity extends AppCompatActivity {
         }else{
 
         }
+        if(StartActivity.name.isEmpty()){
+            TextView Score = (TextView)findViewById(R.id.score);
+            Score.setText("Your score is: " + c);
+        }else{
+            TextView Name = (TextView)findViewById(R.id.congratulation);
+            Name.setText("Congratulation " + name);
+        }
+        if(c == 3){
+            nextButton();
+        }else{
 
-
-
-
+        }
+    }
+    private void nextButton(){
+        Button next = (Button)findViewById(R.id.btnNext);
+        next.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View v){
+                Intent i = new Intent(CupcakeResultActivity.this, CupcakeMiniGame.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
